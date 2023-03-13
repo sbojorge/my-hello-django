40c2170ad79501e077ae1d2188306442d089e58d
sbojorge <sara.bojorge@gmail.com>
2023-03-13 10:18:42 +0000

    Start creating a way for users to add new Todo items

--- todo/views.py
+++ todo/views.py
@@ -11,3 +11,7 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, "todo/todo_list.html", context)
+
+
+def add_item(request):    
+    return render(request, "todo/add_item.html")
