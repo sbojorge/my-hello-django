3fafa64c0d4bbe00930b07afcac6017782a6e007
sbojorge <sara.bojorge@gmail.com>
2023-03-13 16:10:42 +0000

    Add a form that let users to add new items and redirect them back to the home page

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
@@ -13,5 +13,10 @@ def get_todo_list(request):
     return render(request, "todo/todo_list.html", context)
 
 
-def add_item(request):    
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+        return redirect('get_todo_list')
     return render(request, "todo/add_item.html")
