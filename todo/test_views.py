b785f2cab18dad772ba5ce288f4c9f5f548c2932
sbojorge <sara.bojorge@gmail.com>
2023-03-16 11:24:05 +0000

    add tests for the views

---
+++ todo/test_views.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from .models import Item
+
+# Create your tests here.
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
--- todo/tests._views.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-
-class TestDjango(TestCase):
-    def test_this_thing_works1(self):
-        self.assertEqual(1, 1)
